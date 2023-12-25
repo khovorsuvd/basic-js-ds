4f8cd0efc7290561d9a6d69bf746fd94d35e7bd0
khovorsuvd <rrrrr_rrrrrrrrr_rrrrrrr@mail.ru>
2023-12-25 22:28:15 +0300

    fix: full

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.values = [];
+  }
+  
+  push(value) {
+    this.values.push(value);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.values.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.values[this.values.length - 1];
   }
 }
 
